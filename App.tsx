bf66672293da2ff301c1739b09d6dad4bca115b0
Akash Deep <65231036+akashhxthakur@users.noreply.github.com>
2023-01-23 14:51:54 +0530

    sync-indicator

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
 import Header from './components/header';
 import Chart from './components/chart';
 import Creator from './components/creator';
-
+import SyncIndicator from './components/sync-indicator';
 
 const App = () => {
   const [showCreator, setShowCreator] = useState<boolean>(false);
@@ -11,6 +11,7 @@ const App = () => {
     <>
       <StatusBar />
       <SafeAreaView>
+        <SyncIndicator />
         <ScrollView contentInsetAdjustmentBehavior="automatic">
           <Header onOpenCreator={() => setShowCreator(true)} />
           <Creator
